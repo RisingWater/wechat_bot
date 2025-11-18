21d54f4c371e44bf11d2ec8ba8685c7113b49460
risingwater <4680414@qq.com>
2025-11-18 20:58:23 +0800

    message -> name

--- config.py
+++ config.py
@@ -255,7 +255,7 @@ class ConfigManager:
         
         try:
             self._db.insert("dsm_log", log_data)
-            logger.info(f"DSM日志记录添加成功: {timestamp} - {message}")
+            logger.info(f"DSM日志记录添加成功: {timestamp} - {name}")
             return True, "添加成功"
         except Exception as e:
             logger.error(f"添加DSM日志记录失败: {str(e)}")
